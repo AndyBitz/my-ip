8c0880b33bbbf8bf788321da86f7b8bee4a8908a
Andy Bitz <artzbitz@gmail.com>
2022-09-13 18:03:05 +0200

    Use decodeURIComponent on header values

--- output/functions/index.func/index.js
+++ output/functions/index.func/index.js
@@ -1,8 +1,8 @@
 export default function index(request, _event) {
     return new Response(JSON.stringify({
         ip: request.headers.get('x-forwarded-for'),
-        country: request.headers.get('X-Vercel-IP-Country'),
-        city: request.headers.get('X-Vercel-IP-City'),
+        country: decodeURIComponent(request.headers.get('X-Vercel-IP-Country')),
+        city: decodeURIComponent(request.headers.get('X-Vercel-IP-City')),
     }, null, 2), {
         headers: {
             'content-type': 'application/json',
